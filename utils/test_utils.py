85dec695363ff9b91ffb02ea7751fe228ff14b6b
Pavel Savastseika <savaspavel@gmail.com>
2023-05-04 16:00:13 +0400

    add quick_sort and binary_search algos

---
+++ utils/test_utils.py
@@ -0,0 +1,32 @@
+from random import randrange
+from utils import quick_sort, binary_search
+
+
+def test_quick_sort():
+    # test with random numbers
+    n = 100
+    unsorted = [randrange(1_000_000_000) for _ in range(n)]
+
+    expected = sorted(unsorted)
+    actual = quick_sort(unsorted, 0, len(unsorted) - 1, lambda first, second: first < second)
+
+    assert actual == expected
+
+    # test list with indexes
+    n = 100
+    unsorted = [(randrange(1_000_000_000), i) for i in range(n)]
+
+    expected = sorted(unsorted)
+    actual = quick_sort(unsorted, 0, len(unsorted) - 1, lambda first, second: first[0] < second[0])
+
+    assert actual == expected
+
+
+def test_binary_search():
+    sorted_list = [1, 2, 5, 7, 9, 24]
+
+    search_values = [5, 3333, 1, 24]
+    expected_values = [2, -1, 0, 5]
+    actual_values = [binary_search(sorted_list, 0, len(sorted_list) - 1, value) for value in search_values]
+
+    assert actual_values == expected_values
---
+++ utils/utils.py
@@ -0,0 +1,44 @@
+def binary_search(sequence, start, end, value):
+    while start < end:
+        mid = (start + end + 1) >> 1
+        if sequence[mid] > value:
+            end = mid - 1
+        else:
+            start = mid
+
+    if sequence[start] == value:
+        return start
+
+    return -1
+
+
+def quick_sort(sequence, start, end, less):
+    """
+    Sorts given sequence in place. Sequence should contain elements with identical structure.
+    :param sequence: sequence, which will be sorted in place!
+    :param start: left pointer of sequence index
+    :param end: right pointer of sequence index
+    :param less: mini comparator function, which gives True in case first item is strictly less than second.
+    :return: sequence (the same pointer, which was received).
+    """
+    i, j = start, end
+    pivot = sequence[(i + j) >> 1]
+
+    while i <= j:
+        while less(sequence[i], pivot):
+            i += 1
+
+        while less(pivot, sequence[j]):
+            j -= 1
+
+        if i <= j:
+            sequence[i], sequence[j] = sequence[j], sequence[i]
+            i += 1
+            j -= 1
+
+    if start < j:
+        quick_sort(sequence, start, j, less)
+    if i < end:
+        quick_sort(sequence, i, end, less)
+
+    return sequence
